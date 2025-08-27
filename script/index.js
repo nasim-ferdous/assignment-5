723d16df641dc2e2dded28bcc813816a908acf17
nasim-ferdous <nasimferdouspust@gmail.com>
2025-08-27 17:43:15 +0600

    added call button alert function

--- script/index.js
+++ script/index.js
@@ -6,13 +6,20 @@ function getElement(id) {
 document
   .getElementById("all-cards")
   .addEventListener("click", function (event) {
+    // heart icon
     if (event.target.className.includes("heart-icon")) {
       const heartCount = getElement("heart-count").innerText;
       const currentHeartCount = Number(heartCount) + 1;
       getElement("heart-count").innerText = currentHeartCount;
       console.log(currentHeartCount);
     }
+    // call button
     if (event.target.className.includes("call-btn")) {
       const heartIcon = event.target;
+      const serviceName = heartIcon.parentNode.parentNode.children[2].innerText;
+      const serviceNumber =
+        heartIcon.parentNode.parentNode.children[3].innerText;
+      alert("Calling" + " " + serviceName + " " + serviceNumber + "...");
+      console.log(serviceNumber);
     }
   });
